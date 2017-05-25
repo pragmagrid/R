1a65a4930be3d6672f11f633ff6438822f724a2f
nadyawilliams <nadya@sdsc.edu>
2017-05-24 18:13:59 -0700

    mv R distro to google drive.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-(cd src/R; wget https://cran.r-project.org/src/base/R-3/R-3.2.5.tar.gz )
+# pragmagrid google drive
+export SURL="https://googledrive.com/host/0BzgozEGuMWotM21ZeS02dHJYek0"
+. /opt/rocks/share/devel/src/roll/etc/bootstrap-functions.sh
+
+
+#(cd src/R; wget https://cran.r-project.org/src/base/R-3/R-3.2.5.tar.gz )
 
